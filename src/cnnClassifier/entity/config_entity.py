c021bc6d3e9d14348c7fcbba14eed938528fe8dc
Saurabh <kumarsaurabh00435@gamil.com>
2026-07-06 22:17:52 +0530

    prepare base model added

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -7,3 +7,15 @@ class DataIngestionConfig:
     database_name: str
     collection_name: str
     metadata_file: Path
+    
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int    
