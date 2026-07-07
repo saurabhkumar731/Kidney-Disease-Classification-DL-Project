61b4720cf29f3991fe61b5d49dc5c2faa99ce71a
Saurabh <kumarsaurabh00435@gamil.com>
2026-07-07 22:51:43 +0530

    model evaluation added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -3,7 +3,8 @@ import os
 from cnnClassifier.utils.common import read_yaml, create_directories,save_json
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
-                                                TrainingConfig)
+                                                TrainingConfig,
+                                                EvaluationConfig)
                                                 
                                                 
 
@@ -73,4 +74,15 @@ class ConfigurationManager:
             params_image_size=params.IMAGE_SIZE
         )
 
-        return training_config    
+        return training_config  
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion",
+            mlflow_uri="https://dagshub.com/saurabhkumar731/Kidney-Disease-Classification-DL-Project.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config  
