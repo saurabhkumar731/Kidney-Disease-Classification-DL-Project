a2fe0f35e6333de8ddb02c5eb909c6ea90cd9d11
Saurabh <kumarsaurabh00435@gamil.com>
2026-07-09 21:49:14 +0530

    till prediction

--- src/cnnClassifier/pipeline/stage_03_model_training.py
+++ src/cnnClassifier/pipeline/stage_03_model_training.py
@@ -2,6 +2,9 @@ from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.model_training import Training
 from cnnClassifier import logger
 
+import os
+import shutil
+
 
 
 STAGE_NAME = "Training"
@@ -20,6 +23,17 @@ class ModelTrainingPipeline:
         training.train_valid_generator()
         training.train()
 
+        source_model="artifacts/training/model.h5"
+        destination_dir="model"
+        destination_model=os.path.join(destination_dir,"model.h5")
+        
+        os.makedirs(destination_dir,exist_ok=True)
+
+        if os.path.exists(source_model):
+            shutil.copy2(source_model, destination_model)
+            logger.info(f"Model copied successfully to {destination_model}")
+        else:
+            logger.warning(f"Model not found at {source_model}")
 
 
 if __name__ == '__main__':
